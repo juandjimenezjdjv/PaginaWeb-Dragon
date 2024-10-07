d1840c2f5cda51b6a44aa49f5395080346140974
Juan Diego Jiménez Valverde <123849198+juandjimenezjdjv@users.noreply.github.com>
2024-10-07 17:03:29 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -66,7 +66,7 @@ document.addEventListener('DOMContentLoaded', () => {
                         <div class="card-body">
                             <h3 class="card-title">${tour.nombre}</h3>
                             <p class="card-text">Fecha salida: ${new Date(tour.horafechasalida).toLocaleString()}</p>
-                            <p class="card-text">Fecha llegada: ${new Date(tour.horafechasalida).toLocaleString()}</p>
+                            <p class="card-text">Fecha llegada: ${new Date(tour.horafechallegada).toLocaleString()}</p>
                             <p class="card-text"><strong>Precio en colones: ${tour.precio}</strong></p>
                         </div>
                     </a>
